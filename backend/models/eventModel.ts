09af31d756c7bdcef9723bd13268643da6108057
Navi12-18 <91783788+Navi12-18@users.noreply.github.com>
2024-06-30 23:31:58 +0530

    fixed authcontroller

--- backend/models/eventModel.ts
+++ backend/models/eventModel.ts
@@ -11,7 +11,7 @@ const eventSchema: Schema = new Schema({
     title: { type: String, required: true },
     description: { type: String, required: true },
     date: { type: Date, required: true },
-    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'user', required: true },
 });
 
 const Event = mongoose.model<IEvent>('Event', eventSchema);
